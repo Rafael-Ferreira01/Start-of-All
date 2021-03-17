95d3b0aa0e0b6f214a8664bbe718dfc4e5952254
Rafael Oliveira <rafael.171296@outlook.com>
2021-03-17 05:15:37 -0300

    exercicio

---
+++ Start-of-All/refinando o aprendizado/exercicio invertendo numeros.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main ()
+{
+    int num1;
+    int numf;
+
+    printf ("Escreva a primeira ordem\n");
+    scanf ("%i\n", &num1);
+
+    while (num1 < 5; num1++);
+
+    return 0;
+}
